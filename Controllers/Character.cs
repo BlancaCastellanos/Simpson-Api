665a699da2354b8fadab161e8386bf42f1cc0a99
Blanca Castellanos <blancal.castellanos.cbtis37@gmail.com>
2019-10-04 13:23:39 -0700

    Modules codigo

---
+++ Controllers/Character.cs
@@ -0,0 +1,23 @@
+namespace simpsons_api.Modules
+{
+public class Character
+{
+    string firstName;
+    string secondName;
+    string lastName;
+    int age;
+    int height;
+    int weight;
+    string birthDate;
+    string photo;
+    string job;
+    string description;
+public Character(){}
+
+public Character(string firstName)
+{
+
+}
+
+}
+}
