ad7d7168118d68c90b01d6614e6ee2954e8bafa3
Blanca Castellanos <blancal.castellanos.cbtis37@gmail.com>
2019-10-04 13:47:22 -0700

    cambios

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using simpsons_api.Modules;
+using simpsons_api.Dependecies;
+
+namespace simpsons_api.Controllers
+{
+    public 
+}
---
+++ Modules/ICharacter.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+namespace simpson_api.Dependendecies
+{
+    public interface ICharacter
+    {
+        List<Character> GetCharactersList();
+    }
+
+    Character
+}
